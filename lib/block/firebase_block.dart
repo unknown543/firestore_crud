5b4eb6b704359c8139efb7f613da7bd38dfb3969
unknown543 <pratikprajapati858@gmail.com>
2020-07-22 10:54:06 +0530

    modify code

--- lib/block/firebase_block.dart
+++ lib/block/firebase_block.dart
@@ -6,7 +6,11 @@ class FirebaseBlock {
   final _database = Firestore.instance;
   // this method is used for insert only one data into [static document].
   Future<void> insertSingleDocumentUsingMap({Map<String, dynamic> map}) async {
-    await _database.collection("Users").document("Helllo").setData(map);
+    try {
+      await _database.collection("Users").document("Helllo").setData(map);
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   String nameValidation({String data}) {
@@ -31,35 +35,59 @@ class FirebaseBlock {
 
   // this method is used for to insert multipal document into [User] collection.
   Future<void> insertMultipaleDocument({Map<String, dynamic> map}) async {
-    await _database.collection("Users").add(map);
+    try {
+      await _database.collection("Users").add(map);
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   // this method is used for to fetch multipal document into [User] collection.
   Stream<QuerySnapshot> fetchMultipalDocument() {
-    return _database.collection("Users").snapshots();
+    try {
+      return _database.collection("Users").snapshots();
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   // where conditon
   Stream<QuerySnapshot> fetchUSingWhere({String name}) {
-    return _database
-        .collection("Users")
-        .where("name", isEqualTo: name)
-        .limit(1)
-        .snapshots();
+    try {
+      return _database
+          .collection("Users")
+          .where("name", isEqualTo: name)
+          .limit(1)
+          .snapshots();
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   // delete document
   Future<void> deleteDocument({String documentID}) async {
-    return await _database.collection("Users").document(documentID).delete();
+    try {
+      return await _database.collection("Users").document(documentID).delete();
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   // this method is used for to fetch multipal document into [User] collection.
   Stream<DocumentSnapshot> fetchSingleDocument() {
-    return _database.collection("Users").document("Helllo").snapshots();
+    try {
+      return _database.collection("Users").document("Helllo").snapshots();
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   // update single document detail
   Future<void> updateData({String documentId, Map<String, dynamic> map}) async {
-    await _database.collection("Users").document(documentId).updateData(map);
+    try {
+      await _database.collection("Users").document(documentId).updateData(map);
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
